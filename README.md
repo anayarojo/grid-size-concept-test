141161223f2edbda6d246f6a66dbfe8702221ba3
anayarojo <raulmartin_04@hotmail.com>
2020-07-17 21:00:55 -0700

    Add initial version

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # grid-size-concept-test
-Prueba de concepto de diseño responsivo con CSS grid en Safari 
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/Section.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+      <div class="section-content">  
+        {{ title}}
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Section',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style lang="scss">
+
+    section {
+        background-color: #fff;
+
+        .section-content {
+            width: 100%;
+            height: 100%;
+
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+
+            font-weight: 600;
+            font-size: 2.5rem;
+
+            color: rgba(0,0,0,0.25);
+        }
+    }
+
+</style>
